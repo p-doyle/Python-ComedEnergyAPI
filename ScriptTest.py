dafd273d10e7d82d493eccd2b2b90f82c6c61a92
p-doyle <p-doyle@users.noreply.github.com>
2017-07-30 10:02:36 -0500

    Update ScriptTest.py

--- ScriptTest.py
+++ ScriptTest.py
@@ -14,7 +14,7 @@ endDate = datetime.datetime(2017, 06, 01, hour=1)
 # data period can be hour, day or bill
 dataPeriod = 'hour'
 
-usageJson = ComedFunctions.sendUsageRequest(startDate, endDate, dataPeriod, utilityAccountId)
+usageJson = ComedFunctions.sendUsageRequest(authedSession, startDate, endDate, dataPeriod, utilityAccountId)
 
 pprint.pprint(usageJson)
 
